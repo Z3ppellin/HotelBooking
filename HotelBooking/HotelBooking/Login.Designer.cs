fc337710ccb1af1f3e036f86d3dcffcd1fcc431c
jackattack691 <42969124+jackattack691@users.noreply.github.com>
2018-09-04 17:53:47 +0200

    new

--- HotelBooking/HotelBooking/Login.Designer.cs
+++ HotelBooking/HotelBooking/Login.Designer.cs
@@ -34,7 +34,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.btnLogIn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,9 +42,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 77);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Employee no:";
+            this.label1.Text = "User name";
             // 
             // lblCheckIn
             // 
@@ -90,21 +90,22 @@
             this.label3.TabIndex = 17;
             this.label3.Text = "Forgot Password";
             // 
-            // label4
+            // btnLogIn
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(86, 145);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(123, 13);
-            this.label4.TabIndex = 18;
-            this.label4.Text = "*All error text on this form";
+            this.btnLogIn.Location = new System.Drawing.Point(89, 155);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(124, 23);
+            this.btnLogIn.TabIndex = 18;
+            this.btnLogIn.Text = "Sign in";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.button1_Click);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(259, 167);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(259, 203);
+            this.Controls.Add(this.btnLogIn);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
@@ -126,6 +127,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLogIn;
     }
 }
--- HotelBooking/HotelBooking/Login.cs
+++ HotelBooking/HotelBooking/Login.cs
@@ -16,5 +16,10 @@ namespace HotelBooking
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(btnLogIn.Text == "")
+        }
     }
 }
